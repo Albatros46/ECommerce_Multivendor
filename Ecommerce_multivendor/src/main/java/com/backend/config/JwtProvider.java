791701e0008cc7fabfa1134975bd57918360bf7c
SERVET AKCADAG <34627199+Albatros46@users.noreply.github.com>
2025-03-17 23:56:33 +0100

    Otomatik commit

--- Ecommerce_multivendor/src/main/java/com/backend/config/JwtProvider.java
+++ Ecommerce_multivendor/src/main/java/com/backend/config/JwtProvider.java
@@ -6,12 +6,20 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.crypto.SecretKey;
+import java.util.Collection;
 
 public class JwtProvider {
     SecretKey key= Keys.hmacShaKeyFor(JWT_CONSTANT.SECRET_KEY.getBytes());
     public String generateToken(Authentication auth){
 
-        Collation<? extends GrantedAuthority>
-                return null;
+        Collection<? extends GrantedAuthority> authorities=auth.getAuthorities();
+        String roles=populateAuthorities(authorities);
+        
+        return null;
+    }
+
+    private String populateAuthorities(Collection<?  extends GrantedAuthority> authorities) {
+
+        return null;
     }
 }
